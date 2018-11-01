64faf85154b0e55becbf6a5c8ea67ea533172f48
TurboTractopelle <44016081+TurboTractopelle@users.noreply.github.com>
2018-11-01 19:51:10 +0100

    add debut

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import WeatherList from "./components/WeatherList";
+import store from "./Store";
+//const key = "081559a775fda980764c67ca0d45dd1f";
 
 import "./styles.css";
 
-function WeatherApp() {
-  return (
-    <div className="WeatherApp">
-      Weather App
-      <WeatherList />
-    </div>
-  );
+class WeatherApp extends Component {
+  state = { data: store };
+
+  render() {
+    console.log(this.state.data);
+
+    return (
+      <div className="WeatherApp">
+        Weather App
+        <WeatherList />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
